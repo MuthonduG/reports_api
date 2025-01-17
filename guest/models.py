d41955881e93e2170b03fb3822093ce21042b35c
MuthonduG <muthondugithinji@gmail.com>
2025-01-17 03:26:25 -0500

    updates: removed lambda function from guest model

--- guest/models.py
+++ guest/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from datetime import timedelta
 from django.utils.timezone import now
 
+def default_expiry():
+    return now() + timedelta(days=30)
+
 class Guest(models.Model):
     guest_id = models.CharField(max_length=32, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    expiry_at = models.DateTimeField(default=lambda: now() + timedelta(days=30))
+    expiry_at = models.DateTimeField(default=default_expiry)
 
     def is_expired(self):
         return now() > self.expiry_at
